39ddf19f84e69996396d299ae880c26b43f010c1
Hamid <hamidjaved615@gmail.com>
2025-05-09 20:03:15 +0500

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Intern-Design/', // ✅ important for GitHub Pages
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
 })
