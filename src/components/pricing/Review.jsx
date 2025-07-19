4888e4f71335c97e80d96eb9843289318440bfd8
Hamid <hamidjaved615@gmail.com>
2025-07-19 11:39:40 +0500

    Update Review.jsx

--- src/components/pricing/Review.jsx
+++ src/components/pricing/Review.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-// Placeholder Star SVG (Filled Yellow)
+// Placeholder Star SVG (FilYellow)
 const StarIcon = ({ className = "w-5 h-5 sm:w-6 sm:h-6 text-yellow-400" }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
